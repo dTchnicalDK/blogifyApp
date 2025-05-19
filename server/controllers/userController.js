739d0b55c36c4f84b603b55375e74e99e2f49844
dtechnicaldk <dtechnicaldk@gmail.com>
2025-05-19 21:51:59 +0530

    user logout feature added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const tokenSecretCode = process.env.JWT_TOKEN_SECRET;
 
-//function to register user
+//------------------function to register user-----------------------------
 export const registerUser = async (req, res) => {
   let { email, password } = req.body;
   //grab form data
@@ -33,7 +33,7 @@ export const registerUser = async (req, res) => {
   }
 };
 
-//Method to Login user
+//-----------------Method to Login user-----------------------------------
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -65,4 +65,9 @@ export const loginUser = async (req, res) => {
   } catch (error) {
     console.log("user login error", error);
   }
+};
+
+export const userLogOut = (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ msg: "user logged successfully" });
 };
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  loginUser,
+  registerUser,
+  userLogOut,
+} from "../controllers/userController.js";
 const userRouter = Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/logout", userLogOut);
 
 export default userRouter;
